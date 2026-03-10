546c9db79b06b3cb7f32d0ab12cab4d363f70c11
sparkeybranding <sparkeybranding@gmail.com>
2026-03-10 10:40:17 -0500

    Update social sharing thumbnail to clean entrepreneur image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     siteName: "Sparkey! Branding",
     images: [
       {
-        url: "/images/og-image.jpg",
+        url: "/images/og-image-clean.png",
         width: 1200,
         height: 630,
         alt: "Sparkey! Branding - Web Development & Branding",
